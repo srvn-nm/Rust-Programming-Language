33f728cedabe83aa24ce3c28f0384bb550196b1d
sarvin nami <srvn0nm@gmail.com>
2025-07-18 00:07:22 +0330

    running the first Cargo Project

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\Rust Projects\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+D:\Rust Projects\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\Rust\ Projects\hello_cargo\target\debug\hello_cargo.exe: D:\Rust\ Projects\hello_cargo\src\main.rs
