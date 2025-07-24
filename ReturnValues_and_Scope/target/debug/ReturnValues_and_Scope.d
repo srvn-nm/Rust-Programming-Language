088343483006d3e6516d8700abac6b8ab1aab5b5
sarvin nami <srvn0nm@gmail.com>
2025-07-24 12:42:02 +0330

    running and building the project

---
+++ ReturnValues_and_Scope/target/debug/ReturnValues_and_Scope.d
@@ -0,0 +1,1 @@
+D:\Rust\ Projects\ReturnValues_and_Scope\target\debug\ReturnValues_and_Scope.exe: D:\Rust\ Projects\ReturnValues_and_Scope\src\main.rs
---
+++ ReturnValues_and_Scope/target/debug/deps/ReturnValues_and_Scope.d
@@ -0,0 +1,5 @@
+D:\Rust Projects\ReturnValues_and_Scope\target\debug\deps\ReturnValues_and_Scope.d: src\main.rs
+
+D:\Rust Projects\ReturnValues_and_Scope\target\debug\deps\ReturnValues_and_Scope.exe: src\main.rs
+
+src\main.rs:
