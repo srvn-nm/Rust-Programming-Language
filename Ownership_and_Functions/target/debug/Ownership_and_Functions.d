a0876558c083a9527db8e398b2beced69bf2493a
sarvin nami <srvn0nm@gmail.com>
2025-07-24 12:07:06 +0330

    running and building the project

---
+++ Ownership_and_Functions/target/debug/Ownership_and_Functions.d
@@ -0,0 +1,1 @@
+D:\Rust\ Projects\Ownership_and_Functions\target\debug\Ownership_and_Functions.exe: D:\Rust\ Projects\Ownership_and_Functions\src\main.rs
---
+++ Ownership_and_Functions/target/debug/deps/Ownership_and_Functions.d
@@ -0,0 +1,5 @@
+D:\Rust Projects\Ownership_and_Functions\target\debug\deps\Ownership_and_Functions.d: src\main.rs
+
+D:\Rust Projects\Ownership_and_Functions\target\debug\deps\Ownership_and_Functions.exe: src\main.rs
+
+src\main.rs:
